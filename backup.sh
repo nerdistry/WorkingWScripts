a62b6dff62eb30a9a997756c3d0e816ce4515363
Arabella <arabella.mutende@stathmore.edu>
2023-10-06 18:46:57 +0000

    Added my bash scripts.

---
+++ backup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+rsync -av /students/ /students_backup/
+rsync -av /staff/ /staff_backup/
+
---
+++ weather.sh
@@ -0,0 +1,27 @@
+
+#!/bin/bash
+# weather.sh
+# Copyright 2018 computer-geek64. All rights reserved.
+
+program=weatherd
+version=1.1
+year=2023
+developer=bella
+
+case $1 in
+-h | --help)
+	echo "$program $version"
+	echo "Copyright $year $developer. All rights reserved."
+	echo
+	echo "Usage: weather [options]"
+	echo "Option          Long Option             Description"
+	echo "-h              --help                  Show the help screen"
+	echo "-l [location]   --location [location]   Specifies the location"
+	;;
+-l | --location)
+	curl https://wttr.in/$2
+	;;
+*)
+	curl https://wttr.in
+	;;
+esac
